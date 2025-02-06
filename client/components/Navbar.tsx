b7296f41cea8970b60d246470d39f7d653b0b448
ishparsh <uprety.ishparsh@gmail.com>
2025-02-05 22:50:18 -0800

    HTML REACT CSS

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,20 @@
+export const Navbar = () =>{
+    return (
+      <>
+        <div>
+        <header class="header">
+        <h2>Smart Home</h2>
+        <nav>
+          <ul>
+            <li><a href="#">Lights</a></li>
+            <li><a href="#">Temperature</a></li>
+            <li><a href="#">Security</a></li>
+            <li><a href="#">Energy</a></li>
+          </ul>
+        </nav>
+        <p>Welcome, Monkey. D</p>
+      </header>
+        </div>
+    </>
+    )
+};
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,43 @@
 import React, { useEffect, useState } from 'react'
+import {Navbar} from '../components/Navbar'
 
-function index() {
+// function index() {
 
-  const [message, setMessage] = useState("Loading")
-  const [people, setPeople] = useState([]);
+//   const [message, setMessage] = useState("Loading")
+//   const [people, setPeople] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8000/message').then(
-      response => response.json()
-    ).then(
-      data => {
-        setMessage(data.message)
-        setPeople(data.people)
-      }
-    )
-  }, [])
+//   useEffect(() => {
+//     fetch('http://localhost:8000/message').then(
+//       response => response.json()
+//     ).then(
+//       data => {
+//         setMessage(data.message)
+//         setPeople(data.people)
+//       }
+//     )
+//   }, [])
 
+//   return (
+//       <div>{message}</div>
+//       )
+//       }
+
+// export default index
+
+import Head from 'next/head';
+
+export default function Home(){
   return (
-      <div>{message}</div>
-      )
-      }
+    <div>
+      <Head>
+        <title>Next.js App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default index
+      <main>
+        <Navbar />
+        <h1 className="text-4xl font-extrabold text-gray-500">Welcome to my website</h1>
+      </main>
+    </div>
+  );
+}
